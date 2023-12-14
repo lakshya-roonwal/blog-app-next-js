453ea70e9c87539d01ff057849bda4ce3ceecae1
lakshya-roonwal <erenpool7214@gmail.com>
2023-12-14 16:04:14 +0530

    Little style changes

--- app/components/home/BlogsSection.jsx
+++ app/components/home/BlogsSection.jsx
@@ -1,6 +1,4 @@
-"use client";
 import Link from "next/link";
-import React, { useEffect, useState } from "react";
 
 const BlogsSection = ({ blogsdata }) => {
   const blogs=blogsdata;
@@ -43,7 +41,7 @@ const BlogsSection = ({ blogsdata }) => {
                         xmlns="http://www.w3.org/2000/svg"
                       >
                         <path
-                          fill-rule="evenodd"
+                          fileRule="evenodd"
                           d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z"
                           clip-rule="evenodd"
                         ></path>
@@ -81,7 +79,7 @@ const BlogsSection = ({ blogsdata }) => {
                       xmlns="http://www.w3.org/2000/svg"
                     >
                       <path
-                        fill-rule="evenodd"
+                        fileRule="evenodd"
                         d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
                         clipRule="evenodd"
                       ></path>
