8d1bef0088d117466ce02812dc66141a9341dba1
lakshya-roonwal <erenpool7214@gmail.com>
2023-12-19 14:49:17 +0530

    Dark Mode Changes in Privacy Policy and T&C

--- app/privacypolicy/page.js
+++ app/privacypolicy/page.js
@@ -11,60 +11,60 @@ const page = () => {
             </h1>
           </header>
           <br />
-          <span className="font-bold">
+          <span className="font-bold dark:text-slate-100">
             Information Collection and Use:
           </span>{" "}
           <br />
-          Describe the types of personal information you collect (e.g., name,
+          <p className="dark:text-slate-100">Describe the types of personal information you collect (e.g., name,
           email address) and how you collect it (e.g., through contact forms,
           newsletter subscription).State the purpose of collecting this
           information and how you will use it (e.g., to respond to inquiries,
-          provide requested services, improve website content).<br></br>
+          provide requested services, improve website content).</p><br></br>
           <br />
-          <span className="font-bold">Cookies and Tracking Technologies:</span>
+          <span className="font-bold dark:text-slate-100">Cookies and Tracking Technologies:</span>
           <br></br>
-          Explain the use of cookies and other tracking technologies on your
+          <p className="dark:text-slate-100">Explain the use of cookies and other tracking technologies on your
           website.Mention the types of cookies you use (e.g., session cookies,
           analytics cookies) and their purpose.Provide information on how users
-          can manage or disable cookies through their web browser settings.
+          can manage or disable cookies through their web browser settings.</p>
           <br></br>
           <br />
-          <span className="font-bold">Data Sharing:</span>
+          <span className="font-bold dark:text-slate-100">Data Sharing:</span>
           <br></br>
-          Clarify that you will not sell, exchange, or share users&apos; personal
+          <p className="dark:text-slate-100">Clarify that you will not sell, exchange, or share users&apos; personal
           information with third parties without their consent, except as
           required by law.If you do share data with third parties (e.g.,
           analytics tools, advertising networks), explain the purpose and
-          provide links to their respective privacy policies.<br></br>
+          provide links to their respective privacy policies.</p><br></br>
           <br />
-          <span className="font-bold">Data Security:</span>
+          <span className="font-bold dark:text-slate-100">Data Security:</span>
           <br></br>
-          Explain the security measures you have in place to protect user&apos;s
+          <p className="dark:text-slate-100">Explain the security measures you have in place to protect user&apos;s
           personal information from unauthorized access, alteration, disclosure,
           or destruction.State that while you take reasonable precautions, no
           method of data transmission over the internet or electronic storage is
-          100% secure.<br></br>
+          100% secure.</p><br></br>
           <br />
-          <span className="font-bold">Third-Party Websites:</span>
+          <span className="font-bold dark:text-slate-100">Third-Party Websites:</span>
           <br></br>
-          Mention that your website may contain links to external
+          <p className="dark:text-slate-100">Mention that your website may contain links to external
           websites.Clarify that once users leave your website, you are not
           responsible for the privacy practices or content of those third-party
           websites.Encourage users to review the privacy policies of those
-          websites before providing any personal information.<br></br>
+          websites before providing any personal information.</p><br></br>
           <br />
-          <span className="font-bold">Children&apos;s Privacy:</span>
+          <span className="font-bold dark:text-slate-100">Children&apos;s Privacy:</span>
           <br></br>
-          State that your website is not intended for children under the age of
+          <p className="dark:text-slate-100">State that your website is not intended for children under the age of
           13.Declare that you do not knowingly collect or store personal
-          information from children.<br></br>
+          information from children.</p><br></br>
           <br />
-          <span className="font-bold">Changes to the Privacy Policy:</span>
+          <span className="font-bold dark:text-slate-100">Changes to the Privacy Policy:</span>
           <br></br>
-          State that the Privacy Policy may be updated periodically.Explain that
+          <p className="dark:text-slate-100">State that the Privacy Policy may be updated periodically.Explain that
           any changes will be posted on the website with the effective
           date.Encourage users to review the Privacy Policy regularly to stay
-          informed about how their information is being handled.<br></br>
+          informed about how their information is being handled.</p><br></br>
         </article>
       </div>
     </main>
--- app/termsandconditions/page.js
+++ app/termsandconditions/page.js
@@ -11,28 +11,61 @@ const page = () => {
             </h1>
           </header>
           <br />
-          <span className="font-bold">Acceptance of Terms: </span> <br />
-State that by accessing and using the website, users agree to be bound by the Terms & Conditions. <br />
-Clarify that if users do not agree with any part of these terms, they should refrain from using the website. <br />
-<br />
-<span className="font-bold">Intellectual Property: </span> <br />
-State that all content on the website (text, graphics, logos, images, software) is the property of Lakshya Roonwal and protected by intellectual property laws. <br />
-Prohibit users from using, modifying, or distributing any content without prior written permission. <br />
-<br />
-<span className="font-bold">User Conduct: </span> <br />
-Specify that users must use the website only for lawful purposes and in a manner that does not infringe upon the rights of others or restrict their use of the  <br />website.
-Make users solely responsible for any content they post on the website. <br />
-<br />
-<span className="font-bold">Disclaimer: </span> <br />
-State that the website and its content are provided on an &quot;as is&quot; basis without any warranties, expressed or implied. <br />
-Clarify that Lakshya Roonwal does not guarantee the accuracy, reliability, or completeness of the information on the website. <br />
-<br />
-<span className="font-bold">Limitation of Liability: </span> <br />
-Clarify that Lakshya Roonwal and its affiliates will not be liable for any direct, indirect, incidental, consequential, or exemplary damages arising from the use of the website. <br />
-<br />
-<span className="font-bold">Links to Third-Party Websites: </span> <br />
-Mention that the website may contain links to external websites. <br />
-Clarify that Lakshya Roonwal is not responsible for the content or availability of these third-party websites and does not endorse or guarantee their services. <br />
+          <span className="font-bold dark:text-slate-100">
+            Acceptance of Terms:{" "}
+          </span>{" "}
+          <br />
+          <p className="dark:text-slate-100">State that by accessing and using the website, users agree to be bound
+          by the Terms & Conditions. <br />
+          Clarify that if users do not agree with any part of these terms, they
+          should refrain from using the website.</p> <br />
+          <br />
+          <span className="font-bold dark:text-slate-100">
+            Intellectual Property:{" "}
+          </span>{" "}
+          <br />
+          <p className="dark:text-slate-100">State that all content on the website (text, graphics, logos, images,
+          software) is the property of Lakshya Roonwal and protected by
+          intellectual property laws. <br />
+          Prohibit users from using, modifying, or distributing any content
+          without prior written permission. </p><br />
+          <br />
+          <span className="font-bold dark:text-slate-100">
+            User Conduct:{" "}
+          </span>{" "}
+          <br />
+          <p className="dark:text-slate-100">Specify that users must use the website only for lawful purposes and
+          in a manner that does not infringe upon the rights of others or
+          restrict their use of the <br />
+          website. Make users solely responsible for any content they post on
+          the website.</p> <br />
+          <br />
+          <span className="font-bold dark:text-slate-100">
+            Disclaimer:{" "}
+          </span>{" "}
+          <br />
+          <p className="dark:text-slate-100">State that the website and its content are provided on an &quot;as
+          is&quot; basis without any warranties, expressed or implied. <br />
+          Clarify that Lakshya Roonwal does not guarantee the accuracy,
+          reliability, or completeness of the information on the website.</p> <br />
+          <br />
+          <span className="font-bold dark:text-slate-100">
+            Limitation of Liability:{" "}
+          </span>{" "}
+          <br />
+          <p className="dark:text-slate-100">Clarify that Lakshya Roonwal and its affiliates will not be liable for
+          any direct, indirect, incidental, consequential, or exemplary damages
+          arising from the use of the website.</p> <br />
+          <br />
+          <span className="font-bold dark:text-slate-100">
+            Links to Third-Party Websites:{" "}
+          </span>{" "}
+          <br />
+          Mention that the website may contain links to external websites.{" "}
+          <br />
+          <p className="dark:text-slate-100">Clarify that Lakshya Roonwal is not responsible for the content or
+          availability of these third-party websites and does not endorse or
+          guarantee their services.</p> <br />
         </article>
       </div>
     </main>
